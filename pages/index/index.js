14e626d864c77df61b95c4c562e1ad4df91d1505
caojunyan <2433027313@qq.com>
2017-11-29 17:29:56 +0800

    new

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,16 @@
 Page({
-
-})
+    onTap: function () {
+       /* wx.navigateTo({
+            url:"../post/post"
+        });*/
+        wx.redirectTo({
+            url:"../post/post"
+        });
+    },
+    onUnload: function () {
+        console.log("unload")
+    },
+    onHide: function () {
+        console.log("onHide")
+    }
+});
